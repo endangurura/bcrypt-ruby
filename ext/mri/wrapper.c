e1da9f583f229e1df57499086bd39457e1cb57f3
Emmanuel Ndangurura <endangurura@gmail.com>
2019-02-06 08:48:41 +0300

    patch crypt_r for freebsd-12.0

--- ext/mri/wrapper.c
+++ ext/mri/wrapper.c
@@ -175,14 +175,14 @@ char *crypt_ra(const char *key, const char *setting,
 		return NULL;
 	return _crypt_blowfish_rn(key, setting, (char *)*data, *size);
 }
-
+/*
 char *crypt_r(const char *key, const char *setting, void *data)
 {
 	return _crypt_retval_magic(
 		crypt_rn(key, setting, data, CRYPT_OUTPUT_SIZE),
 		setting, (char *)data, CRYPT_OUTPUT_SIZE);
 }
-
+*/
 char *crypt(const char *key, const char *setting)
 {
 	static char output[CRYPT_OUTPUT_SIZE];
